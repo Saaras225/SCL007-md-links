b5464c9d638b531a65dcde74100481ca9bb3d6b7
Saaras225 <sarayance@gmail.com>
2019-02-27 16:10:21 -0300

    se agrega funcion que lee un documento .md y extrae los links

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('fs');
+const path = require('path');
+
+
+fs.readFile('./README.md', function(err,data){
+  if(err){
+      console.log(err);
+  }
+  let stringData = data.toString();
+  
+  let matches;
+  const expre1= new RegExp(/(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/g);
+  matches = stringData.match(expre1);
+  for(let i = 0; i<matches.length; i++)
+      console.log(matches[i])
+  
+})
