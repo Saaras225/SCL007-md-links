57f50e414e66c5fbb07fdb4cd44a890e86fdee0d
Saaras225 <sarayance@gmail.com>
2019-02-28 17:11:16 -0300

    se modifica nombre de getUrl a mdLinks

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = () => {
-
 };
 
   const fs = require('fs');
@@ -7,7 +6,7 @@ module.exports = () => {
   const https = require ("https");
   const fetch = require ("node-fetch");
   
-  const getUrls = (path)=>{
+  const mdLinks = (path)=>{
       fs.readFile(path, function(err,data){
           if(err){
           console.log(err);
@@ -39,16 +38,15 @@ module.exports = () => {
       //console.log(currentPath);
       let files = fs.readdirSync(currentPath);
       for (let i in files) {
-         let currentFile = currentPath + '/' + files[i];
-         let stats = fs.statSync(currentFile);
-         if (stats.isFile()) {
+          let currentFile = currentPath + '/' + files[i];
+          let stats = fs.statSync(currentFile);
+          if (stats.isFile()) {
           let match = reg.exec(currentFile);
           if (match != null)
-          getUrls(currentFile);
-         }
-        else if (stats.isDirectory()) {
-               traverseFileSystem(currentFile);
-             }
-       }
+          mdLinks(currentFile);
+          } else if (stats.isDirectory()) {
+          traverseFileSystem(currentFile);
+          }
+          }
   };
   traverseFileSystem('.');
