8e3d3b53b25eb2ffe271c24691c92268f520b290
Saaras225 <sarayance@gmail.com>
2019-02-28 10:32:54 -0300

    se agrega funcion que valida si las url funcionan o no

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 module.exports = () => {
   
-
-};
-
-const fs = require('fs');
+  const fs = require('fs');
   const path = require('path');
-  
+  const https = require ("https");
+  const fetch = require ("node-fetch");
   
   const getUrls = (path)=>{
-   fs.readFile(path, function(err,data){
+      fs.readFile(path, function(err,data){
           if(err){
           console.log(err);
           }
+  
           let stringData = data.toString();
-          
           let matches;
           const expre1= new RegExp(/(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/g);
           matches = stringData.match(expre1);
-          for(let i = 0; i<matches.length; i++)
-              console.log(matches[i]);
-             
-      
-      
-    })
+          for(let i = 0; i<matches.length; i++){
+              //console.log(matches[i]);
+  
+              fetch(matches[i]).then(function(response){
+                  if (response.ok) {
+                      console.log(matches[i] + " FUNCIONA");
+                  } else {
+                      console.log(response.status, response.statusText);
+                  }
+              })
+              .catch(function(error) {
+              console.log(matches[i]+' ', error.message);
+              });
+          }
+      })
   }
   
   const traverseFileSystem = function (currentPath) {
       let reg = new RegExp(/.+\.md/gi);
       //console.log(currentPath);
-      var files = fs.readdirSync(currentPath);
-      for (var i in files) {
-         var currentFile = currentPath + '/' + files[i];
-         var stats = fs.statSync(currentFile);
+      let files = fs.readdirSync(currentPath);
+      for (let i in files) {
+         let currentFile = currentPath + '/' + files[i];
+         let stats = fs.statSync(currentFile);
          if (stats.isFile()) {
-          var match = reg.exec(currentFile);
+          let match = reg.exec(currentFile);
           if (match != null)
           getUrls(currentFile);
          }
@@ -41,5 +48,6 @@ const fs = require('fs');
                traverseFileSystem(currentFile);
              }
        }
-   };
-    traverseFileSystem('.');
+  };
+  traverseFileSystem('.');
+  };
