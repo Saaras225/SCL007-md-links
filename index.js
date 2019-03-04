a13d9e2438d6d2234991fb5e47abc0ca6da9b938
Saaras225 <sarayance@gmail.com>
2019-03-04 10:15:12 -0300

    se modifica toda la funcion mdLinks, se modifica la funcion que hace la recursion, se modifica el package json, se modifica el readme

--- index.js
+++ index.js
@@ -1,51 +1,78 @@
-module.exports = () => {
-};
-
 const fs = require('fs');
-const path = require('path');
-const https = require ("https");
 const fetch = require ("node-fetch");
 
-const mdLinks = (path)=>{
-    fs.readFile(path,function(err,data){
-        if(err){
-        console.log(err);
-        }
-        let stringData = data.toString();
-        let matches;
-        const expre1= new RegExp(/(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/g);
-        matches = stringData.match(expre1);
-        if(matches!=null){
-            for(let i = 0; i<matches.length; i++){
-            //console.log(matches[i]);
-            fetch(matches[i]).then(function(response){
-                if (response.ok) {
-                    console.log(matches[i] + " FUNCIONA");
-                } else {
-                    console.log(response.status, response.statusText);
-                }
-            })
-            .catch(function(error) {
-                console.log(matches[i]+' ', error.message);
-            });
-        }}
-    })
+module.exports = function mdLinks (path, options){
+    return new Promise((resolve, reject)=>{
+        links = [];
+        Promise.all(traverseFileSystem(path, options.validate)).then((FileLinksList)=>{
+            for(let i in FileLinksList){
+                links = links.concat(FileLinksList[i]);
+            }
+            resolve(links);
+        });
+    });
+    
 }
 
-const traverseFileSystem = function(currentPath){
+ function getLinks(file, validate){  
+    return new Promise((resolve, reject)=>{
+        fs.readFile(file,function(err,data){
+            if(err){
+                resolve([]);
+            }else{
+                //LLEVO EL CONTENIDO DEL ARCHIVO A UN STRING
+                let stringData = data.toString();
+                const expre1= new RegExp(/(\[.+\])\(((https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)\)/g);
+                let match;
+                //INICIALIZO EL ARREGLO DE LINKS VACIO 
+                let linkList = [];
+                let fetchPromises = [];
+                while (match = expre1.exec(stringData)) {
+                    //AGREGO LOS LINKS A LA LISTA, SI ES POSITIVO EL IF AGREGANDO EL TEXTO,EL LINK Y DE DONDE SALE 
+                    if(validate === true)
+                        fetchPromises.push(fetch(match[2]));    
+                    linkList.push({href:match[2], text: match[1], file:file  });                  
+                }
+                    //ESTO AGREGA LA INFORMACION DEL RESULTADO DEL FETCH AL OBJETO
+                    if(validate === true){
+                    Promise.all(fetchPromises).then((responses)=>{
+                        for(let i in responses){
+                            linkList[i].ok = responses[i].ok;
+                            linkList[i].status = responses[i].status;
+                        }
+                        resolve(linkList);
+                    });
+                }else{
+                    resolve(linkList);
+                } 
+            }
+        });
+    });      
+ }
+
+ //ESTO HACE LA RECURSION
+const traverseFileSystem = function(currentPath, validate){
     let reg = new RegExp(/.+\.md/gi);
-        //console.log(currentPath);
     let files = fs.readdirSync(currentPath);
+    let filesPromises = []
+    // recorro los elementos del directorio
     for (let i in files) {
-    let currentFile = currentPath + '/' + files[i];
-    let stats = fs.statSync(currentFile);
-    if (stats.isFile()) {
-    let match = reg.exec(currentFile);
-    if (match != null)
-    mdLinks(currentFile);
-    } else if (stats.isDirectory()) {
-    traverseFileSystem(currentFile);
+        let currentFile = currentPath + '/' + files[i];
+        let stats = fs.statSync(currentFile);
+        //chequeo si el elemento es un archivo o directorio
+        if (stats.isFile()) {
+            // chequeo si es un archivo .md
+            let match = reg.exec(currentFile);
+            if (match != null)
+                // Esto es un archivo .md debo buscar los links
+                filesPromises.push(getLinks(currentFile, validate));
+        } else if (stats.isDirectory()) {
+            // si es directorio me vuelvo a llamar recursivamente
+            filesPromises = filesPromises.concat(traverseFileSystem(currentFile));
+            
+        }
     }
-    }
+    return filesPromises;   
 };
-    traverseFileSystem('.');
+        
+
